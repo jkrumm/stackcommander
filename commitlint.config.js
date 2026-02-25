46130a6be15c40196d950262883f6719eac53a62
jkrumm <jkrumm@pm.me>
2026-02-25 18:03:55 +0100

    fix(ci): disable commitlint body-max-line-length for semantic-release compatibility

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,7 @@
 export default {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    // Disabled: semantic-release generates CHANGELOG commit bodies with long GitHub URLs
+    'body-max-line-length': [0],
+  },
 }
